2d86c619d489ee9f5a7928e2c6e12188214e770f
alex <alex.embrey@apexrms.com>
2021-02-09 11:19:38 -0800

    Remove date/time picker from run control view

--- src/Views/RunControlDataFeedView.Designer.cs
+++ src/Views/RunControlDataFeedView.Designer.cs
@@ -28,33 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.DateTimeStart = new System.Windows.Forms.DateTimePicker();
-            this.DateTimeEnd = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.TextBoxMaxIterations = new System.Windows.Forms.TextBox();
             this.PanelJurisdictions = new SyncroSim.Core.Forms.BasePanel();
             this.label4 = new System.Windows.Forms.Label();
+            this.TextBoxEndDate = new System.Windows.Forms.TextBox();
+            this.TextBoxStartDate = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // DateTimeStart
-            // 
-            this.DateTimeStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.DateTimeStart.Location = new System.Drawing.Point(110, 21);
-            this.DateTimeStart.Name = "DateTimeStart";
-            this.DateTimeStart.Size = new System.Drawing.Size(116, 22);
-            this.DateTimeStart.TabIndex = 1;
-            this.DateTimeStart.ValueChanged += new System.EventHandler(this.DateTimeStart_ValueChanged);
-            // 
-            // DateTimeEnd
-            // 
-            this.DateTimeEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.DateTimeEnd.Location = new System.Drawing.Point(110, 53);
-            this.DateTimeEnd.Name = "DateTimeEnd";
-            this.DateTimeEnd.Size = new System.Drawing.Size(116, 22);
-            this.DateTimeEnd.TabIndex = 3;
-            this.DateTimeEnd.ValueChanged += new System.EventHandler(this.DateTimeEnd_ValueChanged);
             // 
             // label1
             // 
@@ -115,18 +99,52 @@
             this.label4.TabIndex = 6;
             this.label4.Text = "Jurisdictions:";
             // 
+            // TextBoxEndDate
+            // 
+            this.TextBoxEndDate.Location = new System.Drawing.Point(110, 53);
+            this.TextBoxEndDate.Name = "TextBoxEndDate";
+            this.TextBoxEndDate.Size = new System.Drawing.Size(116, 22);
+            this.TextBoxEndDate.TabIndex = 3;
+            // 
+            // TextBoxStartDate
+            // 
+            this.TextBoxStartDate.Location = new System.Drawing.Point(110, 21);
+            this.TextBoxStartDate.Name = "TextBoxStartDate";
+            this.TextBoxStartDate.Size = new System.Drawing.Size(116, 22);
+            this.TextBoxStartDate.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(233, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "(yyyy-MM-dd)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(233, 55);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "(yyyy-MM-dd)";
+            // 
             // RunControlDataFeedView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TextBoxStartDate);
+            this.Controls.Add(this.TextBoxEndDate);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.PanelJurisdictions);
             this.Controls.Add(this.TextBoxMaxIterations);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.DateTimeEnd);
-            this.Controls.Add(this.DateTimeStart);
             this.Name = "RunControlDataFeedView";
             this.Size = new System.Drawing.Size(812, 485);
             this.ResumeLayout(false);
@@ -135,14 +153,15 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.DateTimePicker DateTimeStart;
-        private System.Windows.Forms.DateTimePicker DateTimeEnd;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox TextBoxMaxIterations;
         private Core.Forms.BasePanel PanelJurisdictions;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TextBoxEndDate;
+        private System.Windows.Forms.TextBox TextBoxStartDate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
